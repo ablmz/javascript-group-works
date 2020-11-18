7611d1695a8874e509b41d616994775fb598f58a
ademb <bilmez.adem@stud.hs-hannover.de>
2020-11-18 15:44:10 +0100

    menu and search work without problem

--- js/search.js
+++ js/search.js
@@ -5,3 +5,15 @@
  * 2. Clear the input by clicking the "x" button
  * 
  */
+document.querySelector(".search-btn").addEventListener("click", function1); //we acces the 
+
+function function1(event){
+    event.preventDefault();
+document.getElementById("searchbar").classList.toggle('hidden');
+}
+
+document.getElementById("xbtn").addEventListener("click", function2);
+
+function function2(){
+    document.getElementById("searcharea").value="";
+}
